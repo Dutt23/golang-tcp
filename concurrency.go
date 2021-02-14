e29c45bcc5127318cbb0074b91e3058c3252d96e
Dutt23 <shatyaki_dutt@hotmail.com>
2021-02-14 20:35:31 +0530

    Test code

---
+++ concurrency.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"time"
+)
+
+func main() {
+	go print()
+	// time.Sleep(3 * time.Second)
+	// fmt.Println("main function")
+	channel()
+	time.Sleep(3 * time.Second)
+	fmt.Println("main function")
+}
+
+func print() {
+	fmt.Println("Pringing")
+}
+
+func channel() {
+	c := make(chan int)
+	go stringLength("Salutations", c)
+	go stringLength("World", c)
+	x, y := <-c, <-c
+	fmt.Println(x, y, x+y)
+	errors.New("SISSIH")
+}
+
+func stringLength(s string, c chan int) {
+	c <- len(s)
+}
+func foo() error {
+	return errors.New("Some Error Occurred")
+}
---
+++ hello.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// var count = int(42)
+	// ptr := &count
+	// fmt.Println(*ptr)
+	// *ptr = 100
+	// fmt.Println(count)
+	var guy = new(Person)
+	guy.Name = "Shatyaki"
+	// guy.sayHello()
+	// greet(guy)
+	// foo(guy)
+	// normalForLoop()
+	rangedForLoop()
+}
+
+func normalForLoop() {
+	for i := 0; i < 10; i++ {
+		fmt.Println("Printting ", i)
+	}
+}
+
+func rangedForLoop() {
+	nums := []int{2, 3, 4, 8}
+	for _, val := range nums {
+		fmt.Println("Index value is :", val)
+	}
+}
+
+func foo(i interface{}) {
+	switch v := i.(type) {
+	case int:
+		fmt.Println("INteger value")
+	default:
+		fmt.Printf("Unknown value of type %T", v)
+	}
+}
+
+func (p *Person) SayHello() {
+	fmt.Println("Hello ,", p.Name)
+}
+
+type Friend interface {
+	SayHello()
+}
+
+func greet(f Friend) {
+	f.SayHello()
+}
+
+type Person struct {
+	Name string
+	Age  int
+}
