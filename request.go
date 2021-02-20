ceda6eb84c2ec74e6b7c2dda825eb51d6d179ff9
Dutt23 <shatyaki_dutt@hotmail.com>
2021-02-21 00:00:08 +0530

    Other calls without wrapper

--- request.go
+++ request.go
@@ -2,25 +2,45 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"net/url"
+	"strings"
 )
 
 func main() {
-
 	resp, err := http.Get("http://www.google.com/robots.txt")
+	defer resp.Body.Close()
+	inspectResp(resp)
 	resp, err = http.Head("http://www.google.com/robots.txt")
 	form := url.Values{}
 	form.Add("foo", "test")
+	fmt.Println(form.Encode())
 	resp, err = http.PostForm("https://www.google.com/robots.txt", form)
-	defer resp.Body.Close()
 	fmt.Println(err)
-	// fmt.Println(r1.Body)
+	resp, err = request("PUT", "https://www.google.com/robots.txt", form)
+}
 
-	// body, err := ioutil.ReadAll(r1.Body)
-	// if err != nil {
-	// 	log.Panicln(err)
-	// }
-	// fmt.Println("Reading Body")
-	// fmt.Println(string(body))
+func request(httpType string, url string, body url.Values) (*http.Response, error) {
+	var requestBody io.Reader
+	var client http.Client
+	if body != nil {
+		requestBody = strings.NewReader(body.Encode())
+	}
+	req, err := http.NewRequest(httpType, url, requestBody)
+	resp, err := client.Do(req)
+	return resp, err
+}
+
+func inspectResp(resp *http.Response) {
+	fmt.Println(resp.Status)
+	fmt.Println(resp.StatusCode)
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Panicln(err)
+	}
+	fmt.Println("Reading Body")
+	fmt.Println(string(body))
 }
